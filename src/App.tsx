e5ff64aa9eed907e57a751afcbe8ea4d21b82b46
Chernik1991 <5059585@mail.ru>
2022-04-21 15:57:37 +0300

    lesson 7 reducer finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import { v1 } from 'uuid';
+import {addTaskAC, removeTaskAC, TasksReducers} from './redusers/TasksReducers';
+import {changeFilterAC, FilterReducers} from './redusers/FilterReducers';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
 
-    let [tasks, setTasks] = useState([
+    // let [tasks, setTasks] = useState([
+    //     { id: v1(), title: "HTML&CSS", isDone: true },
+    //     { id: v1(), title: "JS", isDone: true },
+    //     { id: v1(), title: "ReactJS", isDone: false },
+    //     { id: v1(), title: "Rest API", isDone: false },
+    //     { id: v1(), title: "GraphQL", isDone: false },
+    // ]);
+    let [tasks, tasksDispatch] = useReducer(TasksReducers,[
         { id: v1(), title: "HTML&CSS", isDone: true },
         { id: v1(), title: "JS", isDone: true },
         { id: v1(), title: "ReactJS", isDone: false },
@@ -16,17 +25,18 @@ function App() {
     ]);
 
     function removeTask(id: string) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
+        tasksDispatch(removeTaskAC(id))
+        // let filteredTasks = tasks.filter(t => t.id != id);
+        // setTasks(filteredTasks);
     }
 
     function addTask(title: string) {
-        let task = { id: v1(), title: title, isDone: false };
-        let newTasks = [task, ...tasks];
-        setTasks(newTasks);
+        // let task = { id: v1(), title: title, isDone: false };
+        // let newTasks = [task, ...tasks];
+        tasksDispatch(addTaskAC(title));
     }
 
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+    let [filter, filterDispatch] = useReducer(FilterReducers,"all");
 
     let tasksForTodolist = tasks;
 
@@ -38,7 +48,7 @@ function App() {
     }
 
     function changeFilter(value: FilterValuesType) {
-        setFilter(value);
+        filterDispatch(changeFilterAC(value));
     }
 
 
