158a7598ea71044759a7d413fae65fc11a083fdf
MAKS <5059585@mail.ru>
2022-07-11 17:24:35 +0300

    Todolist 8 lesson 1.08 22 04 19

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -3,11 +3,7 @@ import {TodolistType} from '../App';
 export const todolistsReducer=(state:Array<TodolistType>,action:todolistsReducerType)=>{
     switch (action.type){
         case 'REMOVE-TODOLIST':{
-            // setTodolists(todolists.filter(tl => tl.id != id));
-            // // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
-            // delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
-            // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-            // setTasks({...tasks});
+
             return state.filter(el=>el.id!==action.payLoad.todolistId)
         }
         default:return state
