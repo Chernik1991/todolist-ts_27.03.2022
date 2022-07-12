2f8c48eda3bfa57cb0ef6d4151dd4bb1cd42f4bf
MAKS <5059585@mail.ru>
2022-07-12 13:09:02 +0300

    Todolist 8 lesson 1.36 22 04 19

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,23 +1,40 @@
 import {TodolistType} from '../App';
+import {v1} from 'uuid';
 
-export const todolistsReducer=(state:Array<TodolistType>,action:todolistsReducerType)=>{
-    switch (action.type){
-        case 'REMOVE-TODOLIST':{
-
-            return state.filter(el=>el.id!==action.payLoad.todolistId)
+export const todolistsReducer = (state: Array<TodolistType>, action: todolistsReducerType) => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST': {
+            return state.filter(el => el.id !== action.payLoad.todolistId)
         }
-        default:return state
+        case 'ADD-TODOLIST': {
+            let newTodolistId = v1();
+            let newTodolist: TodolistType = {id: newTodolistId, title: action.payLoad.title, filter: 'all'};
+
+            return [...state, newTodolist]
+        }
+        default:
+            return state
     }
 
 }
-type todolistsReducerType=removeTodolistACType;
+type todolistsReducerType = removeTodolistACType | addTodolistACType;
 
-type removeTodolistACType=ReturnType<typeof removeTodolistAC>
-export const removeTodolistAC=(todolistId:string)=>{
-    return{
+type removeTodolistACType = ReturnType<typeof removeTodolistAC>
+export const removeTodolistAC = (todolistId: string) => {
+    return {
         type: 'REMOVE-TODOLIST',
-        payLoad:{
+        payLoad: {
             todolistId
+        }
+    } as const
+}
+
+type addTodolistACType = ReturnType<typeof addTodolistAC>
+export const addTodolistAC = (title: string) => {
+    return {
+        type: 'ADD-TODOLIST',
+        payLoad: {
+            title
         }
     } as const
 }
